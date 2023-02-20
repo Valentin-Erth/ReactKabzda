b34f30c799e7efe64f737836af45ad06388fb760
Kozlov Valentin <kozlov0020@gmail.com>
2023-02-20 13:52:38 +0500

    first commit

---
+++ src/Components/UncontrolledAcor/UncontrolledAcordion.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+type AccorPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+function UncontrolledAccordion(props: AccorPropsType) {
+    debugger
+    console.log("Accor rendering")
+    return (
+        <div>
+            <AccorTitle title={props.titleValue}/>
+            {!props.collapsed && <AccorBody/>}
+        </div>
+    )
+}
+type AccorTitlePropsType = {
+    title: string
+}
+
+function AccorTitle(props: AccorTitlePropsType) {
+    debugger
+    console.log("AccorTitle rendering")
+    return (
+        <h3>---{props.title}---</h3>
+    )
+}
+
+function AccorBody() {
+    console.log("AccorBody rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default Accordion;
