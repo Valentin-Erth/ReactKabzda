427037f3267fcdb307a352e1141d1f247e3a5a32
Kozlov Valentin <kozlov0020@gmail.com>
2023-04-10 16:31:12 +0500

    add stories of usestate

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,22 @@
+import React, {useMemo, useState} from "react";
+
+export default {
+    title: "useState demo",
+}
+function generateData(){
+    //difficult counting
+    console.log("generateData")
+    return 1
+}
+
+export const Example1 = () => {
+    console.log("Example1")
+    //const initialValue=useMemo(generateData,[])
+
+    const [counter,setCounter]=useState(generateData);
+
+    return <>
+        <button onClick={()=>{setCounter(state=>state+1)}}>+</button>
+        {counter}
+    </>
+}
